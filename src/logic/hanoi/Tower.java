138fe5358a25e4cc20254e15a4d859c3ad5464aa
dranjohn <dransheld@outlook.de>
2019-05-11 15:05:09 +0200

    Add plate sorting

--- src/logic/hanoi/Tower.java
+++ src/logic/hanoi/Tower.java
@@ -1,6 +1,7 @@
 package logic.hanoi;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -10,6 +11,16 @@ import java.util.List;
  * @version 1.0
  */
 public class Tower {
+	//determines the sorting oder on this tower
+	private static final Comparator<Plate> PLATE_SORTING_ORDER = new Comparator<Plate>() {
+		@Override
+		public int compare(Plate o1, Plate o2) {
+			//o1 - o2 <=> sort from thin to thick
+			//o2 - o1 <=> sort from thick to thin
+			return o2.getWidth() - o1.getWidth();
+		}
+	};
+	
 	private int dimension;
 	private List<Plate> platesOnThisTower;
 	
@@ -37,5 +48,12 @@ public class Tower {
 	 */
 	public List<Plate> getPlates() {
 		return platesOnThisTower;
+	}
+	
+	/**
+	 * Sorts the plates on the tower. Should be called after every time plates are added or removed.
+	 */
+	private void sortPlates() {
+		platesOnThisTower.sort(PLATE_SORTING_ORDER);
 	}
 }
